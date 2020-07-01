43d65b23b866235aafe6c4b72fc9bc594c48bf86
weathon <waguo6358@gmail.com>
2020-07-01 18:22:01 +0000

    -

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 const express = require('express');
+const { stringify } = require('querystring');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+
+xssdangerous = ['&','/', '<', '>', "'", '"', "$","`"] //因为正则放弃了（） 【】 * ,因为冲突放弃了；#
+// 被打断 顺序
+function axss(astring)
+{
+    for (var i = 0; i < xssdangerous.length; i++) {//forget the g
+        var regex = new RegExp(xssdangerous[i], "g");
+        astring=astring.replace(regex,"&#"+xssdangerous[i].charCodeAt(0)+";")
+    }
+    return astring;
+}
+//放下去
+// app.get('/', function (req, res) {
+//     res.render('index.ejs');
+// });
+
+app.get('/', function (req, res) {
     res.render('index.ejs');
 });
 
-io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
-        socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+io.sockets.on('connection', function (socket) {
+    socket.on('username', function (username) {
+        socket.username = axss(username);
+        io.emit('is_online', '🔵 <i>' + socket.username + ' 加入了群组..</i>');
     });
 
-    socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    socket.on('disconnect', function (username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' 离开了群组..</i>');
     })
 
-    socket.on('chat_message', function(message) {
-        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    socket.on('chat_message', function (message) {
+        io.emit('chat_message', "<strong>" + socket.username + "</strong>: " + axss(message));
     });
 
 });
 
-const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
+const server = http.listen(80, function () {
+    console.log('listening on *:80');
 });
